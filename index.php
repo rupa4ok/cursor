23aaab8417a0b13b934a306e699635063a122abb
rupak <rupak4@yandex.ru>
2019-03-21 15:08:19 +0300

    refactor: 21

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
 <?php
 use App\Router\Router;
-ini_set('default_charset', 'UTF-8');
-mb_internal_encoding("UTF-8");
-ini_set('error_reporting', E_ALL);
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
 define('ROOT', dirname(__DIR__));
 include_once ROOT.'/vendor/autoload.php';
