4986d3367cfbb09d40f1bb6cf25c370c805bb3f5
rupak <rupak4@yandex.ru>
2019-03-21 20:07:43 +0300

    refactor 55

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,5 +2,5 @@
 
 $dir = __DIR__;
 define('ROOT', dirname($dir));
-include_once ROOT.'vendor/autoload.php';
-include_once ROOT.'app/config/config.php';
+include_once ROOT.'/vendor/autoload.php';
+include_once ROOT.'/app/config/config.php';
